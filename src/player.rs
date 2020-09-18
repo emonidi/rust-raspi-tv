945b87339e0e4327e43d0523c0a30ae2050473af
Emiliyan Gospodinov <emonidi@gmail.com>
2020-09-18 14:16:11 +0200

    mvp options

--- src/player.rs
+++ src/player.rs
@@ -19,38 +19,28 @@ impl Player {
         mpv_builder.set_option("vo","opengl").unwrap();
         mpv_builder.set_option("fs","yes").unwrap();
         mpv_builder.set_option("ytdl","yes").unwrap();
+        mpv_builder.set_option("geometry","1920x1080").unwrap();
         mpv_builder.set_option("vd-lavc-threads","4").unwrap();
         mpv_builder.set_option("ontop","no").unwrap();
         mpv_builder.set_option("force-window","yes").unwrap();
         mpv_builder.set_option("osc","yes").unwrap();
         mpv_builder.set_option("hwdec", "rpi-copy").unwrap();
-        // mpv_builder.set_option("osd-level","3").unwrap();
-        // mpv_builder.set_option("opengl-glfinish","yes").unwrap();
-        // mpv_builder.set_option("opengl-swapinterval","0").unwrap();
-        
-        
-        
+        mpv_builder.set_option("scale","ewa_lanczossharp").unwrap();
+        mpv_builder.set_option("cscale","ewa_lanczossharp").unwrap();
+        mpv_builder.set_option("dscale","mitchell").unwrap();
+        mpv_builder.set_option("dither-depth","auto").unwrap();
+        mpv_builder.set_option("correct-downscaling","yes").unwrap();
+        // mpv_builder.set_option("linear-downscaling","yes").unwrap();
+        mpv_builder.set_option("sigmoid-upscaling","yes").unwrap();
+        mpv_builder.set_option("deband","yes").unwrap();
+        mpv_builder.set_option("gpu-api","opengl").unwrap();
+ 
         match mpv_builder.set_option("load-stats-overlay","yes"){
             Ok(r) => {println!("{:?}",r)},
             Err(e) => {println!("{:?}",e)}
         }
 
         let mpv = mpv_builder.build().unwrap();
-
-       
-        // let com = Command::new("mpv")
-        // .env("DISPLAY",":0")
-        // .arg("-fs")
-        // .arg("-vo=gpu")
-        // .arg("--prefetch-playlist")
-        // .arg("--playlist=res/playlist.m3u")
-        // // .arg("--no-terminal")
-        // // .arg("--pause")
-        // .stdin(Stdio::piped())
-        // // .stdout(Stdio::piped())
-        // .spawn()
-        // .unwrap();
-
 
         Command::new("notify-send")
             .arg("Starting TV")
@@ -74,16 +64,16 @@ impl Player {
     //     .spawn()
     //     .expect("");
 
-        let c1 = self.mpv.command(&["loadfile",&channel.url]).unwrap();
-        self.mpv.command(&["show-text",&channel.title]);
+        self.mpv.command(&["loadfile",&channel.url]).unwrap();
+        self.mpv.command(&["show-text",&channel.title]).unwrap();
 
         // {"command":["external-file",""]}
         Player::notify(channel);
     }
 
     pub fn play_prev(&mut self,channel:&Channel){
-        let c1 = self.mpv.command(&["loadfile",&channel.url]).unwrap();
-        self.mpv.command(&["show-text",&channel.title]);
+        self.mpv.command(&["loadfile",&channel.url]).unwrap();
+        self.mpv.command(&["show-text",&channel.title]).unwrap();
        
 
         Player::notify(channel);
